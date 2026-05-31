4197ed0e0acd55a0989a968eff171b7affa9b64f
Bryan-Zavala <bryan.zavala@globant.com>
2026-05-31 02:28:10 +0200

    feat(utils): añade utilidad catchAsync para manejo de errores global

--- controllers/animalesControllers.js
+++ controllers/animalesControllers.js
@@ -1,106 +1,33 @@
-const Animal = require("../models/animalModel");
+const animalService = require("../services/animal.service");
+const catchAsync = require("../utils/catchAsync");
 
-const getAnimales = async (req, res) => {
-  try {
-    // Obtener parámetros de consulta
-    const { ciudad, especie, genero, tamaño, edad } = req.query;
+const getAnimales = catchAsync(async (req, res) => {
+  const animalesFiltrados = await animalService.getAnimales(req.query);
+  return res.status(200).json(animalesFiltrados);
+});
 
-    // Crear objeto de filtro con los parámetros proporcionados
-    const filter = {};
-    if (ciudad) filter.ciudad = ciudad;
-    if (genero) filter.genero = genero;
-    if (tamaño) filter.tamaño = tamaño;
-    if (edad) filter.edad = edad;
+const getAnimalById = catchAsync(async (req, res) => {
+  const animal = await animalService.getAnimalById(req.params.id);
+  return res.status(200).json(animal);
+});
 
-    // Verificar si hay algún parámetro de consulta
-    const hasFilters = Object.keys(filter).length > 0;
+const postAnimal = catchAsync(async (req, res) => {
+  const createdAnimal = await animalService.createAnimal(req.body);
+  return res.status(201).json(createdAnimal);
+});
 
-    let animalesFiltrados;
-    if (hasFilters) {
-      if (especie) {
-        // Dividir los valores de especie por coma y crear un array
-        const especiesArray = especie.split(",");
-        // Filtrar animales para cada valor de especie y combinar los resultados
-        const promises = especiesArray.map((especieValue) =>
-          Animal.find({ ...filter, especie: especieValue })
-        );
-        animalesFiltrados = (await Promise.all(promises)).flat();
-      } else {
-        animalesFiltrados = await Animal.find(filter);
-      }
-    } else {
-      animalesFiltrados = await Animal.find();
-    }
+const putAnimal = catchAsync(async (req, res) => {
+  const updatedAnimal = await animalService.updateAnimal(
+    req.params.id,
+    req.body,
+  );
+  return res.status(200).json(updatedAnimal);
+});
 
-    // Devolver los animales filtrados
-    return res.status(200).json(animalesFiltrados);
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-};
-
-const getAnimalById = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const animal = await Animal.findById(id); // Buscar por la propiedad id
-
-    if (!animal) {
-      return res
-        .status(404)
-        .json({ message: "No se encontró ningún animal con ese ID" });
-    }
-
-    return res.status(200).json(animal);
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-};
-
-const postAnimal = async (req, res) => {
-  try {
-    const newAnimal = new Animal(req.body);
-    const createdAnimal = await newAnimal.save();
-    return res.status(201).json(createdAnimal);
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-};
-
-const putAnimal = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updatedAnimal = await Animal.findByIdAndUpdate(id, req.body, {
-      new: true,
-    });
-
-    if (!updatedAnimal) {
-      return res
-        .status(404)
-        .json({ message: "No tenemos ningún animal con ese ID" });
-    }
-
-    return res.status(200).json(updatedAnimal);
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-};
-
-const deleteAnimal = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deletedAnimal = await Animal.findOneAndDelete(id);
-
-    if (!deletedAnimal) {
-      return res
-        .status(404)
-        .json({ message: "No tenemos ningún animal con ese ID" });
-    }
-
-    return res.status(200).json(deletedAnimal);
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-};
+const deleteAnimal = catchAsync(async (req, res) => {
+  const deletedAnimal = await animalService.deleteAnimal(req.params.id);
+  return res.status(200).json(deletedAnimal);
+});
 
 module.exports = {
   getAnimales,
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,128 +1,48 @@
-const User = require('../models/user.model');
-const bcrypt = require('bcrypt');
-const {validationEmail, validationPassword} = require('../validators/validation');
-const {generateSign} = require('../jwt/jwt');
+const userService = require("../services/user.service");
+const catchAsync = require("../utils/catchAsync");
 
+const register = catchAsync(async (req, res) => {
+  const userData = req.body.user || req.body;
+  const createdUser = await userService.registerUser(userData);
+  return res.status(201).json(createdUser);
+});
 
+const login = catchAsync(async (req, res) => {
+  const data = req.body.user || req.body;
+  const result = await userService.loginUser(data.email, data.password);
+  return res.status(200).json(result);
+});
 
-const register = async(req, res, next) => {
-    try {
-
-        const newUser = new User(req.body.user);
-
-        if(!validationEmail(newUser.email)){
-            res.status(400).send({code:400, message:'Invalid Email'})
-            return next();
-        }
-        if(!(newUser.password)){
-            res.status(400).send({code:400, message:'Invalid password'})
-            return next();
-        }
-
-        const users = await User.find({email:newUser.email})
-        if(users.length > 0){
-            res.status(400).send({code:400, message:'Duplicated Email'})
-            return next();
-        }
-        newUser.password = bcrypt.hashSync(newUser.password, 10);
-        const createdUser = await newUser.save();
-        return res.status(200).json(createdUser);
-    } catch (error) {
-        return res.status(500).json("prooobando" +error);
-    }
-}
-
-const login = async(req, res, next) => {
-
-    const data = req.body.user;
- 
-     try {
-         const user = await User.findOne({email: data.email}).populate("pets favPets inProcessPets");
- 
-         if(user==null){
-             console.log("usuario no encontrado");
-             return res.status(500).json({"errCode":503,"error": "usuario no encontrado"});
-         }
- 
-         if(bcrypt.compareSync(data.password, user.password)){
-             console.log('mi usuario',user);
-             var token = null;
-             try{
-                  token = generateSign(user._id, user.email);
-             }catch (errSign)
-             {
-                 console.log('errSign',errSign);
-             }
- 
-             console.log('este es el token', token);
-             return res.status(200).json({user, token});
-         }else{
-             res.status(400).send({code:400, message:'Password Error'})
-             return next()
-         }
-     } catch (error) {
-         return res.status(500).json(error);
-     }
- }
-
-
-const checkSession =(req, res, next) => {
-    console.log(req.headers.authorization)
-    try {
-        return res.status(200).json(req.user)
-    } catch (error) {
-        return res.status(500).json(error) ;
-    }
+const checkSession = (req, res) => {
+  return res.status(200).json(req.user);
 };
 
-const getUserById = async(req, res) => {
-    try {
-        const {id} = req.params;
-        const myUser = await User.findById(id);
-        if (myUser) {
-            return res.status(200).json(myUser)
-        } else {
-            return res.status(404).json('Any user with that id');
-        }
-    } catch (error) {
-        return res.status(500).json(error);
-    }
-};
+const getUserById = catchAsync(async (req, res) => {
+  const myUser = await userService.getUserById(req.params.id);
+  return res.status(200).json(myUser);
+});
 
-const postFav = async(req, res, next) => {
-    try {
-        const user = new User (req.body)
+const postFav = catchAsync(async (req, res) => {
+  const updatedUser = await userService.updatePopulatedUser(
+    req.body._id,
+    req.body,
+  );
+  return res.status(200).json(updatedUser);
+});
 
-        console.log("new fav", user);
-        const updatedUser = await User.findByIdAndUpdate(user._id, user, {new: true}).populate("pets favPets inProcessPets");
-        console.log("new favorites", user);
-
-        res.status(201).json(updatedUser)
-    } catch (error) {
-        return res.status(500).json(error);
-    }
-}
-
-const postAdoption = async(req, res, next) => {
-    try {
-        const user = new User (req.body)
-
-        console.log("new adoption", user);
-        const updatedUser = await User.findByIdAndUpdate(user._id, user, {new: true}).populate("pets favPets inProcessPets");
-        console.log("new favorites", user);
-
-        res.status(201).json(updatedUser)
-    } catch (error) {
-        return res.status(500).json(error);
-    }
-}
-
+const postAdoption = catchAsync(async (req, res) => {
+  const updatedUser = await userService.updatePopulatedUser(
+    req.body._id,
+    req.body,
+  );
+  return res.status(200).json(updatedUser);
+});
 
 module.exports = {
-    register,
-    login,
-    checkSession,
-    postFav,
-    postAdoption,
-    getUserById
-}
+  register,
+  login,
+  checkSession,
+  postFav,
+  postAdoption,
+  getUserById,
+};
