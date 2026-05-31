2b6a4c828a551f67b1c4ea49ef04dcd95ba78efe
Bryan-Zavala <bryan.zavala@globant.com>
2026-05-31 02:26:38 +0200

    refactor(models): corrige instanciación y limpia sintaxis de esquemas

--- models/user.model.js
+++ models/user.model.js
@@ -1,22 +1,19 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = mongoose.Schema(
-    {
-        name: {type: String, required: false},
-        image: {type:'String',required: false},
-        email: {type: String, required: true},
-        password: {type: String, required: true},
-        pets: [{type: Schema.Types.ObjectId, ref: 'Animal'}],
-        inProcessPets: [{type: Schema.Types.ObjectId, ref: 'Animal'}],
-        favPets: [{type: Schema.Types.ObjectId, ref: 'Animal'}],
-        info: [{type: Schema.Types.ObjectId, ref: 'Forms'}, {required: false}],
-    },
-    {timestamps:true}
+const userSchema = new Schema(
+  {
+    name: { type: String, required: false },
+    image: { type: String, required: false },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    pets: [{ type: Schema.Types.ObjectId, ref: "Animal" }],
+    inProcessPets: [{ type: Schema.Types.ObjectId, ref: "Animal" }],
+    favPets: [{ type: Schema.Types.ObjectId, ref: "Animal" }],
+    info: [{ type: Schema.Types.ObjectId, ref: "Form" }],
+  },
+  { timestamps: true },
 );
 
-
-const User = mongoose.model('user', userSchema);
-
+const User = mongoose.model("User", userSchema);
 module.exports = User;
